f3c8cc97b8d4cfe79db898d78a93d9fd697cad8b
Brian <briankariuki9975@gmail.com>
2026-06-14 17:21:22 +0300

    Fixed biz name not displaying correctly

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
  const params = new URLSearchParams(window.location.search)
   
-  const name = params.get('name') || 'Your Pharmacy'
-  const location1 = params.get('location') || 'Nairobi'
+  const name1 = params.get('name1') || 'Your Pharmacy'
+  const location1 = params.get('location1') || 'Nairobi'
   const phone = params.get('phone') || '+254 700 000 000'
   const exact = params.get("exact") || "444432"
 
-  document.getElementById('biz-name').textContent = name
-  document.getElementById('biz-location').textContent = 'Your Trusted Pharmacy in ' + location
+  document.querySelectorAll('.biz-name').forEach(el => el.textContent = name1)
+  document.querySelectorAll('.biz-location').forEach(el => el.textContent = location1)
   document.getElementById('biz-phone').textContent = phone
   document.getElementById("biz-exact-location").textContent = exact
 
 
-  console.log("Hello World")
+
